da70de8e150ffe7329b0351f8b5cd575d7fe3c18
allen4dev <alanaliagadev@gmail.com>
2017-10-21 23:53:22 -0400

    [Add] base structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="App-intro">
-          Error message, loading spinner and routing.
-        </h1>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1 className="App-intro">
+            Error message, loading spinner and routing.
+          </h1>
+        </div>
+      </Provider>
     );
   }
 }
